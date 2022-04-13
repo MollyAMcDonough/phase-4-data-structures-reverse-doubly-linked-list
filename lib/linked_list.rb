37327e67e98a06bd88e665ba3c11883327bab6f0
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-04-13 18:40:46 -0400

    commit tree 7b851fab5b8c87278e42e092b270485347c91acd

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class LinkedList
   attr_accessor :head, :tail
@@ -10,5 +11,20 @@ class LinkedList
 
   def reverse!
     # your code here
+    if @head
+      if @head.next_node
+        current = self.head
+        prev_node = self.tail
+        self.head = self.tail
+        while current
+          prev_node = current.prev_node
+          current.prev_node = current.next_node
+          current.next_node = prev_node
+          current = current.prev_node
+        end
+        self.tail = prev_node.next_node
+      end
+    end
+    self
   end
 end
